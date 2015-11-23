e3d48cfc2b573e4b650ceadcf066f1f740791afe
Nick Johnstone <ncwjohnstone@gmail.com>
2015-11-23 23:11:59 +1300

    Dispose of old app obserables when code changes
    Otherwise, if you write a counter app, and then change it a little bit,
    shit goes badly!

--- src/scratchpad.js
+++ src/scratchpad.js
@@ -7,15 +7,43 @@ import view from './scratchpad/view';
 import vm from 'vm';
 
 export default function Scratchpad (DOM, props) {
-  props.delay(100).forEach(({code}) => {
+  let sources, sinks;
+  const code$ = DOM.select('.code').events('input')
+    .debounce(300)
+    .map(ev => ev.target.value)
+    .map(code => ({code}));
+
+  props.delay(100).merge(code$).forEach(({code}) => {
+    if (sources) {
+      sources.dispose();
+    }
+
+    if (sinks) {
+      sinks.dispose();
+    }
+
     const context = {div, Observable};
-    const main = vm.runInNewContext(code, context);
+
+    try {
+      vm.runInNewContext(code, context);
+    } catch (e) {
+      console.trace(e);
+    }
+
+    console.log(code);
+
+    if (typeof context.main !== 'function') {
+      return;
+    }
 
     const userDrivers = {
       DOM: makeDOMDriver('.result')
     }
 
     const userApp = run(context.main, userDrivers);
+
+    sources = userApp.sources;
+    sinks = userApp.sinks;
   });
 
   return {
