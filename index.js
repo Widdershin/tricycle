352d6b1d4ef617c7c9e4190579fe4003c44c32da
Nick Johnstone <ncwjohnstone@gmail.com>
2015-11-27 19:40:07 +1300

    Add comment explaining Cycle.run, closes #4

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ import {Observable} from 'rx';
 import Scratchpad from './src/scratchpad';
 
 const startingCode = `
-const {div} = require('@cycle/dom');
+const Cycle = require('@cycle/core');
+const {makeDOMDriver, div} = require('@cycle/dom');
 const _ = require('lodash');
 const {Observable} = require('rx');
 
 
-function main () {
+function main ({DOM}) {
   return {
     DOM: Observable.just(div('.hello-world', 'Hello world!'))
   };
 }
+
+// Normally you need to call Cycle.run, but Tricycle handles that for you!
+// If you want to try this out locally, just uncomment this code.
+//
+// Cycle.run(main, {
+//  DOM: makeDOMDriver('.app')
+// });
 `;
 
 function main ({DOM}) {
